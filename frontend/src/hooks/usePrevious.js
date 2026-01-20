f55c900f6b5f8f7f2e29b07d43bc2a26fdbfb3fc
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2026-01-20 12:28:57 +0100

    feat(hooks): add usePrevious hook for tracking prior values

---
+++ frontend/src/hooks/usePrevious.js
@@ -0,0 +1,18 @@
+import { useRef, useEffect } from 'react';
+
+/**
+ * Hook to get the previous value
+ * @param {any} value - Current value
+ * @returns {any} - Previous value
+ */
+export const usePrevious = (value) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+};
+
+export default usePrevious;
