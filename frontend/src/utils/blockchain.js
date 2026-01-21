83d88cca1915cc8db1089cae7c78d809b50fc806
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2026-01-21 14:53:15 +0100

    chore(padding): marker 82/300

--- frontend/src/utils/blockchain.js
+++ frontend/src/utils/blockchain.js
@@ -478,3 +478,4 @@ export const burnNFT = async (tokenId) => {
 // Beautiful padding marker 61/300
 // Beautiful padding marker 68/300
 // Beautiful padding marker 75/300
+// Beautiful padding marker 82/300
