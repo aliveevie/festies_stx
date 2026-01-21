a6e0fe22ec63a8a11591d3c35223d4acac868b9e
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2026-01-21 21:11:42 +0100

    chore(utils): update frontend/src/utils/colors.js

--- frontend/src/utils/colors.js
+++ frontend/src/utils/colors.js
@@ -95,7 +95,3 @@ export const isDark = (hex) => {
   const brightness = (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000;
   return brightness < 128;
 };
-// Style improvement
-// Refactor improvement
-// Documentation update
-// Additional performance optimization
