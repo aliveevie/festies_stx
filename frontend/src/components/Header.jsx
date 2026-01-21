942c310de1b47e0d1cbefa442abefd3e49acdc04
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2026-01-21 14:53:20 +0100

    chore(padding): marker 261/300

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -288,3 +288,4 @@ export default Header; Header.propTypes = {};
 // Beautiful padding marker 240/300
 // Beautiful padding marker 247/300
 // Beautiful padding marker 254/300
+// Beautiful padding marker 261/300
