1faf62fdafbcffd793a4daf836ea322c099e22d6
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2026-01-21 14:53:21 +0100

    chore(padding): marker 291/300

--- frontend/src/utils/environment.js
+++ frontend/src/utils/environment.js
@@ -210,3 +210,4 @@ export const isDebugMode = () => {
 // Beautiful padding marker 270/300
 // Beautiful padding marker 277/300
 // Beautiful padding marker 284/300
+// Beautiful padding marker 291/300
